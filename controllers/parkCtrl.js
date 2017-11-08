42712db45cfbd5c3a36f156e9fc34fb63a7b3412
Jules Sepulveda <julietrsepulveda@gmail.com>
2017-11-08 15:29:56 -0600

    correct updated twitter request

--- controllers/parkCtrl.js
+++ controllers/parkCtrl.js
@@ -35,10 +35,12 @@ module.exports.getSinglePark = (req, res, next) => {
     .then( (data) => {
       park = data;
       getTweets(park)
+      // getParkHashtag(park)
       .then( (mediaUrl) => {
         const {dataValues:Park} = park;
         data = park.dataValues.Park;
-        res.render('park-details', { data , mediaUrl})
+        // res.render('park-details', { data , mediaUrl})
+        res.json(mediaUrl)
       })
     })
     .catch(err => {
@@ -49,8 +51,9 @@ module.exports.getSinglePark = (req, res, next) => {
 let getTweets = (req, res, next) => {
   let tweetMedia = [];
   let screen_name = park.screenName;
-  // var url = `https://api.twitter.com/1.1/search/tweets.json?q=%40${screen_name}%2Bfilter%3Aimages&count=25&include_entities=true&tweet_mode=extended`;
-  let url = `https://api.twitter.com/1.1/search/tweets.json?q=${screen_name}%2Bfrom%3A${screen_name}%2Bfilter%3Aimages&count=20&include_entities=true&tweet_mode=extended`;
+  console.log("PARK FULL NAME", park.Park.fullName)
+  // let url = `https://api.twitter.com/1.1/search/tweets.json?q=${screen_name}%2Bfrom%3A${screen_name}%2Bfilter%3Aimages&count=50&include_entities=true&tweet_mode=extended`; //WORKING QUERY
+  var url = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=GlacierNPS&filter%3Amedia&count=2&include_entities=true&tweet_mode=extended`;
 
   var tweetsInfo = {
     uri: url,
@@ -60,25 +63,65 @@ let getTweets = (req, res, next) => {
     },
     json: true
   };
+  console.log("API REQUEST", tweetsInfo)
   return rp(tweetsInfo)
   .then(function (body) {
-      let status = body.statuses;
-      console.log("STATUS". status)
-      let entries;
-    status.forEach(function(statuses){
-      let newMedia = statuses.entities.media
-      // console.log("NEW MEDIA", newMedia)
-      newMedia.forEach(function(data){
-        let mediaUrl = data.media_url_https;
-        tweetMedia.push(mediaUrl);
-      })
-    })
-    return tweetMedia
+    console.log("TWEET DATA", body)
+    // -----> access to url: body[0].entities.media[0].media_url_https
+      let twitterData = body;
+
+      // console.log("STATUS", twitterData)
+    // twitterData.forEach(function(statuses){
+    //   console.log("STATUSES", statuses)
+    //   let newMedia = statuses.entities
+    //   console.log("NEW MEDIA", newMedia)
+    // //   newMedia.forEach(function(data){
+    // //     let mediaUrl = data.media_url_https;
+    // //     tweetMedia.push(mediaUrl);
+    // //   })
+    // })
+    // console.log("TWEET MEDIA ARRAY", tweet)
+    return twitterData
   })
   .catch(function (err) {
     console.log(err)
   })
 }
+
+// let getParkHashtag = (req, res, next) => {
+//   let tweetMedia = [];
+//   let screen_name = park.screenName;
+//   console.log("PARK FULL NAME", park.Park.fullName)
+//   // var url = `https://api.twitter.com/1.1/search/tweets.json?q=%40${screen_name}%2Bfilter%3Aimages&count=25&include_entities=true&tweet_mode=extended`;
+//   let url = `https://api.twitter.com/1.1/search/tweets.json?q=${screen_name}%2Bfrom%3A${screen_name}%2Bfilter%3Aimages&count=50&include_entities=true&tweet_mode=extended`;
+
+//   var tweetsInfo = {
+//     uri: url,
+//     headers: {
+//       'User-Agent': 'Request-Promise',
+//       "Authorization": "Bearer " + bearerToken
+//     },
+//     json: true
+//   };
+//   return rp(tweetsInfo)
+//   .then(function (body) {
+//       let status = body.statuses;
+//       // console.log("STATUS". status)
+//       // let entries;
+//     status.forEach(function(statuses){
+//       let newMedia = statuses.entities.media
+//       // console.log("NEW MEDIA", newMedia)
+//       newMedia.forEach(function(data){
+//         let mediaUrl = data.media_url_https;
+//         tweetMedia.push(mediaUrl);
+//       })
+//     })
+//     return tweetMedia
+//   })
+//   .catch(function (err) {
+//     console.log(err)
+//   })
+// }
 
 // adds park to favorites table in db
 module.exports.savePark = (req, res, next) => {
