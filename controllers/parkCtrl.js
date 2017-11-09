c7e24bc0ba25d1bd101304ac28ca28b4625aef27
Jules Sepulveda <julietrsepulveda@gmail.com>
2017-11-09 17:22:00 -0600

    print park list by state name

--- controllers/parkCtrl.js
+++ controllers/parkCtrl.js
@@ -8,35 +8,6 @@ var bearerToken = process.env.TWITTER_BEARER_TOKEN; //the bearer token obtained 
 let natParks = [];
 let parkState = {};
 
-let sortedParks = {
-  'AK': { state:"Alaska", parks: []},
-  'AR': { state:"Arkansas", parks: []},
-  'AZ': { state:"Arizona", parks: []},
-  'CA': { state:"California", parks: []},
-  'CO': { state:"Colorado", parks: []},
-  'FL': { state:"Florida", parks: []},
-  'HI': { state:"Hawaii", parks: []},
-  'ID': { state:"Idaho", parks: []},
-  'KY': { state:"Kentucky", parks: []},
-  'ME': { state:"Maine", parks: []},
-  'MI': { state:"Michigan", parks: []},
-  'MN': { state:"Minnesota", parks: []},
-  'MT': { state:"Montana", parks: []},
-  'NC': { state:"North Carolina", parks: []},
-  'ND': { state:"North Dakota", parks: []},
-  'NM': { state:"New Maexico", parks: []},
-  'NV': { state:"Nevada", parks: []},
-  'OH': { state:"Ohio", parks: []},
-  'OR': { state:"Oregon", parks: []},
-  'SC': { state:"South Carolina", parks: []},
-  'SD': { state:"South Dakota", parks: []},
-  'TX': { state:"Texas", parks: []},
-  'UT': { state:"Utah", parks: []},
-  'VA': { state:"Virginia", parks: []},
-  'VI': { state:"Virgin Islands", parks: []},
-  'WA': { state:"Washington", parks: []},
-  'WY': { state:"Wyoming", parks: []}
-};
 // gets information of all parks
 module.exports.getParks = (req, res, next) => {
   const { Park } = req.app.get('models');
@@ -45,23 +16,46 @@ module.exports.getParks = (req, res, next) => {
     order: ['states']
   })
   .then( (parkData) => {
-    for(var i = 0; i < parkData.length; i++) {
-      natParks.push(parkData[i])// adds all park info to array
-    }
-    natParks.forEach( (park) => {
-      let obj = {};
-      
-    console.log("PARKS?", sortedParks[park.states.split(',')[0]])
+    let sortedParks = {
+      'AK': { state:"Alaska", parks: []},
+      'AR': { state:"Arkansas", parks: []},
+      'AZ': { state:"Arizona", parks: []},
+      'CA': { state:"California", parks: []},
+      'CO': { state:"Colorado", parks: []},
+      'FL': { state:"Florida", parks: []},
+      'HI': { state:"Hawaii", parks: []},
+      'ID': { state:"Idaho", parks: []},
+      'KY': { state:"Kentucky", parks: []},
+      'ME': { state:"Maine", parks: []},
+      'MI': { state:"Michigan", parks: []},
+      'MN': { state:"Minnesota", parks: []},
+      'MT': { state:"Montana", parks: []},
+      'NC': { state:"North Carolina", parks: []},
+      'ND': { state:"North Dakota", parks: []},
+      'NM': { state:"New Maexico", parks: []},
+      'NV': { state:"Nevada", parks: []},
+      'OH': { state:"Ohio", parks: []},
+      'OR': { state:"Oregon", parks: []},
+      'SC': { state:"South Carolina", parks: []},
+      'SD': { state:"South Dakota", parks: []},
+      'TX': { state:"Texas", parks: []},
+      'UT': { state:"Utah", parks: []},
+      'VA': { state:"Virginia", parks: []},
+      'VI': { state:"Virgin Islands", parks: []},
+      'WA': { state:"Washington", parks: []},
+      'WY': { state:"Wyoming", parks: []}
+    };
+
+    parkData.forEach( (park) => {
+    // console.log("PARKS?", sortedParks[park.states.split(',')[0]])
     
       sortedParks[park.states.split(',')[0]].parks.push(park);
 
     });
-    console.log("SORTED PARKS", sortedParks)
+    // console.log("SORTED PARKS", sortedParks)
+    // return sortedParks;
+    res.render('parks', { sortedParks }) 
     })
-    .then( () => {
-      res.render('parks', { sortedParks }) 
-      return natParks;
-  })
   .catch( (err) => {
     next(err);
   }); 
